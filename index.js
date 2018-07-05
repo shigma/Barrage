99ed81c2b0cde2b67887ab0c01b365926ee805f4
githubshn <js4418@126.com>
2018-07-05 22:40:49 +0800

    加入了一个黄色的方块
    假装那是无敌的自机

--- index.js
+++ index.js
@@ -11,13 +11,25 @@ new Vue({
       barrages: [],
       active: null,
       stopTime: 0,
-      lastTime: 0
+      lastTime: 0,
+      x: 0,
+      y: 0,
+      aleft: false, aright: false, aup: false, adown: false,
+      v: 5,
     }
   },
 
   mounted() {
     const canvas = this.$refs.canvas
+
+    this.x = this.$refs.canvas.width/2,
+    this.y = this.$refs.canvas.height/8*7,
+
     this.context = canvas.getContext('2d')
+
+    document.addEventListener("keydown",event => this.accelerate(event,true));
+    document.addEventListener("keyup",event => this.accelerate(event,false));
+
     this.addBarrage(require('./barrages/bar1'))
     this.barrages.forEach(barrage => barrage.mutate())
   },
@@ -36,6 +48,9 @@ new Vue({
     display(timestamp) {
       this.context.clearRect(0, 0, this.$refs.canvas.width, this.$refs.canvas.height)
       this.barrages.forEach(barrage => barrage.mutate(timestamp - this.stopTime))
+
+      this.updateself();
+
       this.active = requestAnimationFrame(this.display)
     },
     toggle() {
@@ -47,6 +62,58 @@ new Vue({
         this.stopTime += performance.now() - this.lastTime
         this.active = requestAnimationFrame(this.display)
       }
+    },
+    accelerate(event,dir){
+      switch(event.key){
+        case 'ArrowLeft':{//left
+          this.aleft=dir;
+          break;
+        }
+        case 'ArrowUp':{//up
+          this.aup=dir;
+          break;
+        }
+        case 'ArrowRight':{//right
+          this.aright=dir;
+          break;
+        }
+        case 'ArrowDown':{//down
+          this.adown=dir;
+          break;
+        }
+      }
+    },
+    updateself(){
+      var speed = this.v;
+      var ss = 0;
+      var sizex = 10, sizey = 10;
+      var scrw = this.$refs.canvas.width, scrh = this.$refs.canvas.height;
+
+      if(this.aup||this.adown)ss++;
+      if(this.aright||this.aleft)ss++;
+      speed/=Math.sqrt(ss);
+      
+      if(this.aleft){
+        this.x-=speed;
+      }
+      if(this.aright){
+        this.x+=speed;
+      }
+      if(this.aup){
+        this.y-=speed;
+      }
+      if(this.adown){
+        this.y+=speed;
+      }
+      
+      if(this.x<0)this.x=0;
+      if(this.y<0)this.y=0;
+      if(this.x>scrw-sizex)self.x=scrw-sizex;
+      if(this.y>scrh-sizey)self.y=scrh-sizey;
+
+      this.context.fillStyle = 'yellow';
+      this.context.fillRect(this.x,this.y,sizex,sizey);
+
     }
   },
 
@@ -56,6 +123,7 @@ new Vue({
       <button @click="toggle">
         <div>{{ active ? 'Pause' : active === null ? 'Start' : 'Resume' }}</div>
       </button>
+      </div><p>{{x}}</p><p>{{aup}}</p>
     </div>
   </div>`
 })
